31927f115046ee5f66a2a2bcd1520212f82da375
Anton Persson <pltxtra@vuknob.com>
2020-02-08 10:56:30 +0100

    Added new knob editor class.
    Signed-off-by: Anton Persson <pltxtra@vuknob.com>

---
+++ src_jni/ui_code/knob_editor.cc
@@ -0,0 +1,84 @@
+/*
+ * vu|KNOB
+ * (C) 2020 by Anton Persson
+ *
+ * http://www.vuknob.com/
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License version 2; see COPYING for the complete License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#include "knob_editor.hh"
+
+/***************************
+ *
+ *  Class KnobEditor::Knobinstance
+ *
+ ***************************/
+
+auto KnobEditor::KnobInstance::create_sequence(
+	KammoGUI::GnuVGCanvas::ElementReference &container,
+	KammoGUI::GnuVGCanvas::ElementReference &template,
+	std::shared_ptr<RISequence> ri_sequence,
+	int offset) -> std::shared_ptr<Sequence> {
+
+	char bfr[32];
+	snprintf(bfr, 32, "seq_%p", ri_sequence.get());
+
+	KammoGUI::GnuVGCanvas::ElementReference new_graphic =
+		root.add_element_clone(bfr, sequence_graphic_template);
+
+	SATAN_DEBUG("Sequencer::Sequence::create_sequence() -- bfr: %s\n", bfr);
+
+	auto new_sequence = std::make_shared<Sequence>(new_graphic, ri_sequence, offset);
+	ri_sequence->add_sequence_listener(new_sequence);
+	return new_sequence;
+}
+
+/***************************
+ *
+ *  Class KnobEditor
+ *
+ ***************************/
+
+KnobEditor::KnobEditor(KammoGUI::GnuVGCanvas* cnvs)
+	: SVGDocument(std::string(SVGLoader::get_svg_directory() + "/knobEditor.svg"), cnvs)
+{
+	knob_container = KammoGUI::GnuVGCanvas::ElementReference(this, "knobContainer");
+	knob_template = KammoGUI::GnuVGCanvas::ElementReference(this, "knobTemplate");
+	root = KammoGUI::GnuVGCanvas::ElementReference(this);
+}
+
+void KnobEditor::on_resize() {
+	auto canvas = get_canvas();
+	canvas->get_size_pixels(canvas_w, canvas_h);
+	canvas->get_size_inches(canvas_w_inches, canvas_h_inches);
+
+	double tmp;
+
+	tmp = canvas_w_inches / INCHES_PER_FINGER;
+	canvas_width_fingers = (int)tmp;
+	tmp = canvas_h_inches / INCHES_PER_FINGER;
+	canvas_height_fingers = (int)tmp;
+
+	tmp = canvas_w / ((double)canvas_width_fingers);
+	finger_width = tmp;
+	tmp = canvas_h / ((double)canvas_height_fingers);
+	finger_height = tmp;
+
+	// get data
+	root.get_viewport(document_size);
+	scaling = finger_height / document_size.height;
+}
+
+void KnobEditor::on_render() {
+}
---
+++ src_jni/ui_code/knob_editor.hh
@@ -0,0 +1,65 @@
+/*
+ * VuKNOB
+ * Copyright (C) 2020 by Anton Persson
+ *
+ * http://www.vuknob.com/
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef VUKNOB_KNOB_EDITOR
+#define VUKNOB_KNOB_EDITOR
+
+#include <map>
+#include <gnuVGcanvas.hh>
+
+class KnobEditor
+	: public RemoteInterface::Context::ObjectSetListener<RISequence>
+	, public KammoGUI::GnuVGCanvas::SVGDocument
+{
+
+private:
+	class KnobInstance
+		: public std::enable_shared_from_this<PatternInstance>
+	{
+	private:
+		KammoGUI::GnuVGCanvas::ElementReference svg_reference;
+
+	public:
+		KnobInstance(
+			KammoGUI::GnuVGCanvas::ElementReference &elref
+			);
+
+		virtual ~KnobInstance() {
+			svg_reference.drop_element();
+		}
+
+	};
+
+	KammoGUI::GnuVGCanvas::ElementReference root, knob_container, knob_template;
+	KammoGUI::GnuVGCanvas::SVGRect document_size;
+
+	double scaling; // graphical scaling factor
+	double finger_width = 10.0, finger_height = 10.0; // sizes in pixels
+	int canvas_width_fingers = 8, canvas_height_fingers = 8; // sizes in "fingers"
+	float canvas_w_inches, canvas_h_inches; // sizes in inches
+	int canvas_w, canvas_h; // sizes in pixels
+
+public:
+	KnobEditor(KammoGUI::GnuVGCanvas* cnvs);
+
+	virtual void on_resize() override;
+	virtual void on_render() override;
+};
+
+#endif
